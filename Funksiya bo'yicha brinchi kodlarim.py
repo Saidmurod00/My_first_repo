95b9879e2c930d89c22ff4b24280a9aaa514d9b2
Saidmurod00 <Cobalttt911@gmail.com>
2024-08-21 10:53:39 +0500

    Funksiya bo'yicha brinchi kodlarim
    Bu yangi kodlar funksiya bo'yicha darsda bajarilgan

---
+++ Funksiya bo'yicha brinchi kodlarim.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 21 10:35:37 2024
+
+@author: Saidmurod00
+"""
+
+#Turli hil sodda funksiyalar
+
+def salom_ber():
+    """Chaqirsangiz salom beradi"""
+    print("Assalomu aleykum")
+    
+salom_ber() 
+
+def alik_ol():
+    """Chaqirsangiz alik oladi"""
+    print("Voaleykum assalom")
+    
+alik_ol()
+
+def mening_ismim():
+    """Mening ismim"""
+    print("Mening ismim Saidmurod")
+    
+mening_ismim()
+
+def tanishuv():
+    """Tanishganidan hursanligini aytadi"""
+    print("Tanishganimdan hursandman")
+    
+tanishuv()
+
+def tanishuv_yoqmadi():
+    """Tanishuv yoqmasa"""
+    print("Tanishuvdan xursand emasman")
+    
+#tanishuv_yoqmadi()
+
+def yoshingiz_nechida():
+    """Yoshini sorash"""
+    print("Yoshingiz nechida?")
+    
+yoshingiz_nechida()
+
+def mening_yoshim():
+    """Ozimning yoshimni aytaman"""
+    print("Mening yoshim 20 da 2004-yilman")
+    
+mening_yoshim()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
